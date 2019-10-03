b45c37e3a65df9fd19af5ddaaa99544650a43d9f
Pako Herrera <pakman_198@hotmail.com>
2019-10-03 11:11:33 -0700

    Fetch data from the API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
 
-import CatalogProvider from './CatalogProvider';
 import ProductList from './ProductList';
-import ProductDetail from './ProductDetail';
-import CatalogContext from './CatalogContext';
 import Header from './Header';
 
 const App = () => {
     return (
-      <CatalogProvider>
         <div className="ui container">
           <Header />
-          <CatalogContext.Consumer>
-            {
-              ({selectedProduct}) => {
-                return selectedProduct ? <ProductDetail /> : null
-              }
-            }
-          </CatalogContext.Consumer>
           <ProductList />
         </div>
-      </CatalogProvider>
     );
 }
 
